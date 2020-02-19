144c79d83e7a02f630a174ebd0e10ec7cf73ccff
DELL <DELL@DESKTOP-FRIMMDM>
2020-02-19 13:07:56 +0530

    Online Wallet

---
+++ TestProject/src/Models/WalletAccount.java
@@ -0,0 +1,60 @@
+package Models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import Models.WalletTransactions;
+
+public class WalletAccount
+{
+	int accountId;
+	double accountBalance;
+	enum status
+	{
+		PENDING,APPROVED,REJECT;
+	}
+	
+	List<WalletTransactions> TransactionHistory=new ArrayList<WalletTransactions>();
+	
+	
+		
+
+	public WalletAccount(int accountId, double accountBalance) 
+	{
+		super();
+		this.accountId = accountId;
+		this.accountBalance = accountBalance;
+	}
+
+
+	public int getAccountId()
+	{
+		return accountId;
+	}
+	
+	public void setAccountId(int accountId)
+	{
+		this.accountId = accountId;
+	}
+	
+	public double getAccountBalance()
+	{
+		return accountBalance;
+	}
+	
+	public void setAccountBalance(double accountBalance)
+	{
+		this.accountBalance = accountBalance;
+	}
+	
+	public List<WalletTransactions> getTransactionHistory() 
+	{
+		return TransactionHistory;
+	}
+	
+	public void setTransactionHistory(List<WalletTransactions> transactionHistory)
+	{
+		TransactionHistory = transactionHistory;
+	}
+	
+}
